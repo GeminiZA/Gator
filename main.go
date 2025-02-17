19e88c248d6b9fa855a877056142e7ec6b0d807b
Tristan Theron <tristantheron@gmail.com>
2025-02-17 14:34:09 +0200

    CH2L5

--- main.go
+++ main.go
@@ -1,26 +1,152 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/GeminiZA/Gator/internal/config"
+	"github.com/GeminiZA/Gator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
+type state struct {
+	cfg *config.Config
+	db  *database.Queries
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	funcs map[string]func(*state, command) error
+}
+
 func main() {
+	// Read cfg
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
-	cfg.SetUser("tristan")
-	cfg, err = config.Read()
+	// Set up database
+	db, err := sql.Open("postgres", cfg.DbUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	data, err := json.MarshalIndent(cfg, "", "  ")
+	queries := database.New(db)
+	// Initialize State
+	curState := state{
+		cfg: cfg,
+		db:  queries,
+	}
+	// Register commands
+	cmds := commands{
+		funcs: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
+
+	// Run command
+	if len(os.Args) < 2 {
+		log.Fatal("too few arguments")
+	}
+	cmdArgs := os.Args[2:]
+	err = cmds.run(&curState, command{
+		os.Args[1],
+		cmdArgs,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", data)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.funcs[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if f, ok := c.funcs[cmd.name]; ok {
+		err := f(s, cmd)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("command not found")
+	}
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("invalid arguments")
+	}
+	userName := cmd.args[0]
+	user, err := s.db.GetUser(context.Background(), userName)
+	if err != nil {
+		return err
+	}
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("invalid arguments")
+	}
+	userName := cmd.args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      userName,
+	})
+	if err != nil {
+		return err
+	}
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("User \"%s\" successfully registered:\n%v\n", user.Name, user)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("invalid arguments")
+	}
+	err := s.db.Reset(context.Background())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("invalid arguments")
+	}
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		if user == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user)
+		} else {
+			fmt.Printf("* %s\n", user)
+		}
+	}
+	return nil
 }
